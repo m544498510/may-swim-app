c467609cb2f7e8c663e02b7c1bd38bc8c5ca2f06
May <m544498510@gmail.com>
2016-08-02 18:08:10 +0800

    路由基本完成

--- src/main/webWorkSpace/src/mainApp/index.js
+++ src/main/webWorkSpace/src/mainApp/index.js
@@ -10,15 +10,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore, applyMiddleware,compose,combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory,hashHistory } from 'react-router'
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
 
-import otherPage from './otherPage';
-import login from './otherPage/login';
-import register from './otherPage/register';
+import OtherFrame from './otherPage/OtherFrame';
+import OtherFrame2 from './otherPage/OtherFrame2';
+import Login from './otherPage/login/Login';
+import Register from './otherPage/register/Register';
 
 const middleware = process.env.NODE_ENV === 'production' ?
   [thunk] :
@@ -34,17 +35,11 @@ const store = createStore(
   )
 );
 // Create an enhanced history that syncs navigation events with the store
-const history = syncHistoryWithStore(browserHistory, store);
+const history = syncHistoryWithStore(hashHistory, store);
+
 
 render(
   <Provider store={store}>
-    { /* Tell the Router to use our enhanced history */ }
-    <Router history={history}>
-      <Route path="/" component={otherPage}>
-        <Route path="/login" component={login} />
-        <Route path="/register" component={register} />
-      </Route>
-    </Router>
   </Provider>,
   document.getElementById('root')
 );
