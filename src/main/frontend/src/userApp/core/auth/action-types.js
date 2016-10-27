44ea809089097dd86eee064f88062437bf39b357
May <m544498510@gmail.com>
2016-10-27 23:21:45 +0800

    add action and reducer in auth module

---
+++ src/main/frontend/src/userApp/core/auth/action-types.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * @author :    Mei XinLin
+ * @version :   1.0
+ */
+'use strict';
+
+export const SIGN_IN_SUCCESS = 'SIGN_IN_SUCCESS';
+export const SIGN_IN_ERROR = 'SIGN_IN_ERROR';
---
+++ src/main/frontend/src/userApp/core/auth/reducer.js
@@ -0,0 +1,8 @@
+/**
+ *
+ * @author :    Mei XinLin
+ * @version :   1.0
+ */
+'use strict';
+
+import {Recode} from 'immutable';
