277539ee00d696aec897f05c69fb81f209970c03
May <m544498510@gmail.com>
2016-11-15 16:14:58 +0800

    增加gulp-env，用于控制环境变量。

--- src/main/frontend/gulpTask/default.js
+++ src/main/frontend/gulpTask/default.js
@@ -5,27 +5,27 @@
  */
 'use strict';
 
-import gulp         from 'gulp';
-import babel        from 'gulp-babel';
-import runSequence  from 'run-sequence';
+import gulp from 'gulp';
+import runSequence from 'run-sequence';
 import gutilsModule from 'gulp-load-utils';
+import env from 'gulp-env';
 
 const config = require(process.cwd() + '/config');
 
 const gutils = gutilsModule(['colors', 'env', 'log']);
 
 
-gulp.task('default',()=>{
+gulp.task('default', ()=> {
   /*
    * 用于获取输入的环境信息，并声明出来。（默认环境为development）
    * Run `gulp --production`
    */
   var type = gutils.env.production ? 'production' : 'development';
-
-  if (type == "development") {
-    runSequence('clean',['build:style','build:html','assets','lib','build:js'],'dist','complete');
-  } else if (type == "production") {
-    runSequence(['clean','unitTest'], ['build:js--production', 'build:style--production', 'min:img','move:nls','move:lib'], 'build:html--production','dist', 'complete');
+  env.set({NODE_ENV: type});
+  if (type == 'development') {
+    runSequence('clean', ['build:style', 'build:html', 'assets', 'lib', 'build:js'], 'dist', 'complete');
+  } else if (type == 'production') {
+    runSequence(['clean', 'unitTest'], ['build:js--production', 'build:style--production', 'min:img', 'move:nls', 'move:lib'], 'build:html--production', 'dist', 'complete');
   } else {
     gutils.log(gutils.colors.green('////////////////////////////////////'));
     gutils.log(gutils.colors.green('///                              ///'));
