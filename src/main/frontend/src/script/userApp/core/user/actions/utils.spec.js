552b85610c87055da754c35cddcf7b836f68a078
May <m544498510@gmail.com>
2016-11-20 22:43:27 +0800

    action单元测试

---
+++ src/main/frontend/src/script/userApp/core/user/actions/utils.spec.js
@@ -0,0 +1,22 @@
+/**
+ *
+ * @author :    Mei XinLin
+ * @version :   1.0
+ */
+'use strict';
+
+import utils from './utils';
+
+describe('user action utils', ()=>{
+  describe('function validateEmail',()=>{
+    it('return true when email is a valid data',()=>{
+      const flag = utils.validateEmail('123@qq.com');
+      expect(flag).to.be.true;
+    });
+
+    it('return false when email is a invalid data',()=>{
+      const flag = utils.validateEmail('123');
+      expect(flag).to.be.false;
+    });
+  });
+});
