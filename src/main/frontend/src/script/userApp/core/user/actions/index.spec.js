5aaef1b8c784d25c64de9f04095cf9cc6e09ffd1
May <m544498510@gmail.com>
2016-11-23 22:33:05 +0800

    测试代码调整

--- src/main/frontend/src/script/userApp/core/user/actions/index.spec.js
+++ src/main/frontend/src/script/userApp/core/user/actions/index.spec.js
@@ -15,74 +15,51 @@ const middleware = [thunk];
 const mockStore = configureStore(middleware);
 
 describe('User action', ()=> {
+  let _store, _initialState;
+  before(function () {
+    _initialState = {
+      user: {
+        email: '123@qq.com',
+        password: '123',
+        secondPsd: '123'
+      }
+    };
+    _store = mockStore(_initialState);
+  });
+
+  afterEach(function () {
+    _store.clearActions();
+  });
   describe('signUp', ()=> {
-
     it('should dispatch when email and password is validate', ()=> {
-
-      const initialState = {
-        user:{
-          email: '123@qq.com',
-          password: '123',
-          secondPsd: '123'
-        }
-      };
-      const store = mockStore(initialState);
-
-      store.dispatch(actions.signUp());
-
-      const actionArr = store.getActions();
+      _store.dispatch(actions.signUp());
+      const actionArr = _store.getActions();
 
       const expectedPayload = {
         type: actionTypes.SIGN_UP,
-        payload: initialState.user
+        payload: _initialState.user
       };
       expect(actionArr).to.deep.equal([expectedPayload]);
 
     });
 
-    it('should dispatch nothing when email is invalidate',()=>{
-      const initialState = {
-        user: {
-          email: '123',
-          password: '123',
-          secondPsd: '123'
-        }
-      };
-      const store = mockStore(initialState);
-
-      store.dispatch(actions.signUp());
-      const actionArr = store.getActions();
-
+    it('should dispatch nothing when email is invalidate', ()=> {
+      _store.dispatch(actions.signUp());
+      const actionArr = _store.getActions();
       expect(actionArr).to.be.empty
     });
 
-    it('should dispatch nothing when password is invalidate',()=>{
-      const initialState = {
-        user: {
-          email: '123@qq.com',
-          password: '1234',
-          secondPsd: '123'
-        }
-      };
-      const store = mockStore(initialState);
-
-      store.dispatch(actions.signUp());
-      const actionArr = store.getActions();
-
+    it('should dispatch nothing when password is invalidate', ()=> {
+      _store.dispatch(actions.signUp());
+      const actionArr = _store.getActions();
       expect(actionArr).to.be.empty
-
     })
-
-
   });
 
-  describe('emailChange', ()=>{
-    it('should dispatch null string emailErrorInfo with validate email',()=>{
-      const store = mockStore({});
-
-      store.dispatch(actions.emailChange('123@qq.com'));
-      const actionArr = store.getActions();
-
+  describe('emailChange', ()=> {
+    it('should dispatch null string emailErrorInfo with validate email', ()=> {
+      _store.dispatch(actions.emailChange('123@qq.com'));
+      const actionArr = _store.getActions();
       const expectedPayload = {
         type: actionTypes.CHECK_EMAIL_FULFILLED,
         payload: {
@@ -94,11 +71,9 @@ describe('User action', ()=> {
 
     });
 
-    it('should dispatch "邮箱格式不正确" emailErrorInfo with invalidate email',()=>{
-      const store = mockStore({});
-
-      store.dispatch(actions.emailChange('123'));
-      const actionArr = store.getActions();
+    it('should dispatch "邮箱格式不正确" emailErrorInfo with invalidate email', ()=> {
+      _store.dispatch(actions.emailChange('123'));
+      const actionArr = _store.getActions();
 
       const expectedPayload = {
         type: actionTypes.CHECK_EMAIL_FULFILLED,
@@ -113,16 +88,10 @@ describe('User action', ()=> {
   });
 
 
-  describe('secondPsdChange',()=>{
-    it('should dispatch null string secondPsdErrorInfo when password is validate',()=>{
-      const store = mockStore({
-        user: {
-          password: '123'
-        }
-      });
-
-      store.dispatch(actions.secondPsdChange('123'));
-      const actionArr = store.getActions();
+  describe('secondPsdChange', ()=> {
+    it('should dispatch null string secondPsdErrorInfo when password is validate', ()=> {
+      _store.dispatch(actions.secondPsdChange('123'));
+      const actionArr = _store.getActions();
 
       const expectedPayload = {
         type: actionTypes.SECOND_PASSWORD_CHANGE,
@@ -135,20 +104,14 @@ describe('User action', ()=> {
 
     });
 
-    it('should dispatch "两次输入的密码不一致" secondPsdErrorInfo when password is invalidate',()=>{
-      const store = mockStore({
-        user: {
-          password: '1234',
-        }
-      });
-
-      store.dispatch(actions.secondPsdChange('123'));
-      const actionArr = store.getActions();
+    it('should dispatch "两次输入的密码不一致" secondPsdErrorInfo when password is invalidate', ()=> {
+      _store.dispatch(actions.secondPsdChange('1234'));
+      const actionArr = _store.getActions();
 
       const expectedPayload = {
         type: actionTypes.SECOND_PASSWORD_CHANGE,
         payload: {
-          secondPsd: '123',
+          secondPsd: '1234',
           secondPsdErrorInfo: '两次输入的密码不一致'
         }
       };
