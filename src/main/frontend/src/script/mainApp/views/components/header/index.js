a6ac8b1709b589f6e34eaacbb8fba041d8bf409b
May <m544498510@gmail.com>
2016-11-30 17:18:14 +0800

    component header

--- src/main/frontend/src/script/mainApp/views/components/header/index.js
+++ src/main/frontend/src/script/mainApp/views/components/header/index.js
@@ -5,6 +5,8 @@
  */
 'use strict';
 import React, {PropTypes} from 'react';
+import {Dropdown,MenuItem} from 'react-bootstrap';
+import Toggle from 'widgets/dropdown/toggle';
 
 import {Link} from 'react-router';
 
@@ -20,8 +22,32 @@ const Header = ()=>{
         <icon className="fa fa-bars" />
       </a>
       <div className="header_userProfile_box">
-        <img className="userInfo_pic"
-          src="http://beijing678.oss-cn-beijing.aliyuncs.com/jeemaa/documents/user/19251/face/1b06449747124c069887320865f4d480.jpg" alt=""/>
+        <Dropdown id="userProfileDropdown" pullRight={true}>
+          <Toggle  bsRole="toggle">
+            <img className="user_profile_pic"
+                 src="http://beijing678.oss-cn-beijing.aliyuncs.com/jeemaa/documents/user/19251/face/1b06449747124c069887320865f4d480.jpg" alt=""/>
+          </Toggle>
+          <Dropdown.Menu className="user_profile_dropdown_menu">
+            <MenuItem>
+              <Link to="/user/profile" >
+                <icon className="fa fa-user" />
+                我的信息
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <Link to="/user/profile" >
+                <icon className="fa fa-cog" />
+                设置
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <icon className="fa fa-power-off" />
+              退出
+            </MenuItem>
+          </Dropdown.Menu>
+        </Dropdown>
+
+
       </div>
 
     </header>
