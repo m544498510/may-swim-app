af9b3ccc661d1b80ead574c4aea5e2c9f7e0343b
May <m544498510@gmail.com>
2016-12-02 17:03:29 +0800

    sidebar起头

---
+++ src/main/frontend/src/script/mainApp/views/components/sidebar/index.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author :    Mei XinLin
+ * @version :   1.0
+ */
+'use strict';
+
+import React,{Component,PropTypes} from 'react';
+
+export default class Sidebar extends Component{
+
+
+
+}
