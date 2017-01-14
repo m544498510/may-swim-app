6d35322a187a5f2f6c520abe2033f9887727b50e
May <m544498510@gmail.com>
2017-01-14 15:39:15 +0800

    test config change

--- src/main/frontend/test/test-bundle.js
+++ src/main/frontend/test/test-bundle.js
@@ -8,13 +8,11 @@
 import 'babel-polyfill'
 import chai from 'chai'
 import sinonChai from 'sinon-chai'
-import chaiAsPromised from 'chai-as-promised'
 import chaiEnzyme from 'chai-enzyme'
 import fetchMock from 'fetch-mock';
 
 
 chai.use(sinonChai);
-chai.use(chaiAsPromised);
 chai.use(chaiEnzyme());
 
 global.chai = chai;
