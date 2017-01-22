1b8047f0f3aa821c0400447d5ea3def5a9a42eae
May <m544498510@gmail.com>
2017-01-22 17:27:45 +0800

    main component

---
+++ src/main/frontend/src/script/mainApp/views/components/main/index.js
@@ -0,0 +1,52 @@
+/**
+ *
+ * @author :    Mei XinLin
+ * @version :   1.0
+ */
+
+import React, {Component, PropTypes} from 'react';
+import {Link} from 'react-router';
+
+export default class Main extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    breadcrumb: PropTypes.arrayOf(PropTypes.object).isRequired,
+  };
+
+  render() {
+    return (
+      <main>
+        <div className="main-frame">
+          <div className="main-header clearfix">
+            <h2>{this.props.title}</h2>
+            <ul className="breadcrumb">
+              {renderBreadcrumb(this.props.breadcrumb)}
+            </ul>
+          </div>
+          <div className="main-content">
+            {this.props.children}
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+function renderBreadcrumb(list) {
+  const itemArr = [];
+  for (let i = 0; i < list.length; i++) {
+    const item = list[i];
+    let linkTag;
+    if (item.link) {
+      linkTag = <Link to={item.link}>{item.name}</Link>;
+    } else {
+      linkTag = item.name;
+    }
+    itemArr.push(
+      <li className="breadcrumb-item">
+        {linkTag}
+      </li>
+    );
+  }
+  return itemArr;
+}
